892a0b0b7bb858561fb0bd815fdbf4d7ec085813
anissavaughn <anissa.vaughnn@gmail.com>
2025-05-22 18:06:49 -0400

    add changes to main.tsx sent by TA

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,21 @@
+// code provided by Tisha
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import store from "./kambaz/store"; 
+import "bootstrap/dist/css/bootstrap.min.css";
+ 
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
+
+/*
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -11,4 +29,5 @@ createRoot(document.getElementById("root")!).render(
  </StrictMode>
 );
 
+*/
 
